b18e5d39ba6105588caec2a27dbea47d5780ca80
Sripad Kulkarni <sripad.kulkarni@sripadk-ltm772b.internal.salesforce.com>
2024-09-01 20:48:41 +0530

    testing

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = App(
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
+@app.message("help")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>! Please open a support ticket at help.heroku.com. Thank you!")
